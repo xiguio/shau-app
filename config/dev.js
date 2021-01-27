95b447bd6b61139223dadc87303ba2b6ab5d474a
唐文雍 <tammeny@qq.com>
2021-01-27 11:08:12 +0800

    Update version

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 const config =  {
 	api: 'http://127.0.0.1:7001',
 	imageDomain: 'https://img.xigu.pro',
-	wechatMPVersion: 106,
+	wechatMPVersion: 107,
 }
 export default config;
--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 const config =  {
 	api: 'https://14d-api.xigu.pro',
 	imageDomain: 'https://img.xigu.pro',
-	wechatMPVersion: 106,
+	wechatMPVersion: 107,
 }
 export default config;
